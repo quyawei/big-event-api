1da260ae22c314637491a81ffbec0fa5eceff907
quyawei <quyawei1@outlook.com>
2020-07-09 21:55:39 +0800

    初始化版本

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.listen(8888, () => { 
+    console.log('running....')
+})
+
+app.get('/data', (req,res) => {
+    res.send('hello')
+})
