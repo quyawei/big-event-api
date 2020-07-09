5e7b9434b5067a3770df786649368440fa10e968
quyawei <quyawei1@outlook.com>
2020-07-09 22:24:31 +0800

    完善入口文件

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
+
+// 处理客户端请求post参数
+// for parsing application/json
+app.use(express.json()) 
+// for parsing application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true })) 
+
+// 设置跨域
+app.use(cors())
 
 app.listen(8888, () => { 
     console.log('running....')
