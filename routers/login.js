cab3ebe3a5043ef6212f059e5ace84d114045503
quyawei <quyawei1@outlook.com>
2020-07-10 21:05:12 +0800

    密码加密

--- routers/login.js
+++ routers/login.js
@@ -3,6 +3,7 @@
 */
 const express = require('express')
 const path = require('path')
+const utils = require('utility')
 const db = require(path.join(__dirname,'../common/db.js'))
 const router = express.Router()
 
@@ -14,6 +15,8 @@ router.post('/login', (req, res) => {
 router.post('/reguser', async (req, res) => { 
     // 获取前端传递过来的参数
     let param = req.body
+    // 对客户端传递过来的密码加密后在进行数据库的插入操作
+    param.password = utils.md5(req.body.password)
     // 调用数据库相关方法进行数据添加操作
     let sql = 'insert into user set ?'
     let ret = await db.operateData(sql, param)
