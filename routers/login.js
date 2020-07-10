d498813d5bd8ea84af4f78e575e7247f75a8db0d
quyawei <quyawei1@outlook.com>
2020-07-10 20:46:11 +0800

    实现注册用户接口

--- routers/login.js
+++ routers/login.js
@@ -10,8 +10,27 @@ router.post('/login', (req, res) => {
     res.send('login')
 })
 
-router.post('/reguser', (req, res) => { 
-    res.send('reguser')
+// 注册用户接口
+router.post('/reguser', async (req, res) => { 
+    // 获取前端传递过来的参数
+    let param = req.body
+    // 调用数据库相关方法进行数据添加操作
+    let sql = 'insert into user set ?'
+    let ret = await db.operateData(sql, param)
+    // 如果ret非空，再去获取他的属性，防止报错
+    // affectedRows属性作用表示此次操作影响了几行数据
+    if (ret && ret.affectedRows > 0) {
+        res.json({
+            status: 0,
+            message: '注册成功'
+        })
+    } else { 
+        res.json({
+            status: 1,
+            message: '注册失败'
+        })
+    }
+    
 })
 
 router.get('/test', async (req, res) => { 
