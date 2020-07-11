affdc6408af230f36882510bf75bfa7b41980b30
quyawei <quyawei1@outlook.com>
2020-07-12 01:16:58 +0800

    获取用户信息

--- routers/user.js
+++ routers/user.js
@@ -2,21 +2,36 @@
     用户信息相关接口
 */
 const express = require('express')
-
 const router = express.Router()
+const path = require('path')
+const db = require(path.join(__dirname,'../common/db.js'))
 
 // 路由配置
 // 获取用户信息
 router.get('/userinfo', (req, res) => { 
-    res.send('userinfo')
-})
-
-// 更新用户信息
-router.post('/userinfo', (req, res) => { 
     // 根据什么获取用户的详细信息？用户的id
     // 用户的id从哪里得到？token
     // req.user表示从token中获取的信息，该信息是登录成功后放入的
     // req.user属性名称是固定的吗？是的
+    // 根据用户的id查询用户的详细信息
+    let sql = 'select id,username,nickname,email,user_pic from user where id = ?'
+    let info = await db.operateData(sql,req.user.id)
+    if (info && info.length > 0) {
+        res.json({
+            status: 0,
+            message: '获取用户基本信息成功',
+            data: info[0]
+        })
+    } else { 
+        res.json({
+            status: 1,
+            message: '获取用户信息失败',
+        })
+    }
+})
+
+// 更新用户信息
+router.post('/userinfo',async (req, res) => { 
     res.send('update userinfo')
 })
 
